20bf87b6f2a043ee1577fadfe910054579b7ba17
VVBphp <bliskunvv@yandex.ru>
2025-01-30 23:10:50 +0300

    init

--- src/YandexSmartCaptchaServiceProvider.php
+++ src/YandexSmartCaptchaServiceProvider.php
@@ -8,20 +8,20 @@ class YandexSmartCaptchaServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-//        $this->app->singleton('yandex-smart-captcha', function () {
-//            return new Services\YandexSmartCaptchaService(
-//                config('yandex-smart-captcha.client_key'),
-//                config('yandex-smart-captcha.server_key')
-//            );
-//        });
+        $this->app->singleton('yandex-smart-captcha', function () {
+            return new Services\YandexSmartCaptchaService(
+                config('yandex-smart-captcha.client_key'),
+                config('yandex-smart-captcha.server_key')
+            );
+        });
     }
 
     public function boot(): void
     {
-//        $this->publishes([
-//            __DIR__.'/../config/yandex-smart-captcha.php' => config_path('yandex-smart-captcha.php'),
-//        ]);
-//
-//        $this->loadViewsFrom(__DIR__.'/../resources/views', 'yandex-smart-captcha');
+        $this->publishes([
+            __DIR__.'/../config/yandex-smart-captcha.php' => config_path('yandex-smart-captcha.php'),
+        ]);
+
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'yandex-smart-captcha');
     }
 }
